7e8a753fa7b315146fbfc8d23327afa156ef4977
Nahid Ahmed <86636618+NahidAhmed07@users.noreply.github.com>
2025-01-15 09:33:19 +0600

    first commit

---
+++ src/routes/__root.tsx
@@ -0,0 +1,20 @@
+import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+
+export const Route = createRootRoute({
+  component: () => (
+    <main className="p-2 border flex-1">
+      <header className="p-2 flex gap-2">
+        <Link to="/" className="[&.active]:font-bold">
+          Home
+        </Link>{" "}
+        <Link to="/about" className="[&.active]:font-bold">
+          About
+        </Link>
+      </header>
+      <hr />
+      <Outlet />
+      <TanStackRouterDevtools />
+    </main>
+  ),
+});
